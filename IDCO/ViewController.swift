4c5ce6c25d911209e5205c0303eccfd19dd20f1a
Leighton Rangi <Leightonr39@hotmail.com>
2020-05-28 15:33:29 +1200

    initial xcode project creation

---
+++ IDCO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IDCO
+//
+//  Created by Leighton  Rangi on 28/05/20.
+//  Copyright © 2020 Leighton  Rangi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
